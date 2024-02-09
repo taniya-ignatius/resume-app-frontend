8857633fc1d8aa0eb334ba5bea68d1a961a88067
taniya-ignatius <itaniya895@gmail.com>
2024-02-09 23:44:34 +0530

    Signin success

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Add from './components/Add';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <BrowserRouter>
      <Routes>
       <Route path='/' element={<Add/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
      </Routes>
      </BrowserRouter>
     </div>
