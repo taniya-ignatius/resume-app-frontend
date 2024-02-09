4dadd84a45c9928c076e10081c4dfbeb790a4e22
taniya-ignatius <itaniya895@gmail.com>
2024-02-09 23:36:11 +0530

    Signup success with navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+            <nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">RESUME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">SIGNUP</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/view">SIGNIN</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/view">ADD RESUME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/view">VIEW RESUMES</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default NavBar
